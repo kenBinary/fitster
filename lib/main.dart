bb59be2afbefe5351edabe511b03bd4e478c3192
tanwa1 <duanyotanwa@gmail.com>
2023-11-24 19:46:25 +0800

    changed the padding of google button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:fitster/loginpage.dart';
+import 'package:fitster/screens/loginpage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
