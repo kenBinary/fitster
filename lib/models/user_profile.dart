15d088dad605ebe75a056a3ac8ec065ccb7269e6
thennek0 <kennethbecaro0@gmail.com>
2023-11-24 12:22:17 +0800

    add toJson and workout data initialize method

--- lib/models/user_profile.dart
+++ lib/models/user_profile.dart
@@ -5,13 +5,13 @@ class UserProfile {
   int height;
   int weight;
   int exerciseDays = 0;
-  Map<int, Map<int, int>> workoutDates = {
+  Map<int, Map<int, int>> _workoutDates = {
     2023: {
       11: 23,
     },
   };
 
-  Map<String, double> weeklyCalorieBurned = {
+  Map<String, double> _weeklyCalorieBurned = {
     'Mon': 0,
     'Tue': 0,
     'Wed': 0,
@@ -38,6 +38,27 @@ class UserProfile {
   }
 
   void updateCalorieBurned(String key, double value) {
-    weeklyCalorieBurned[key] = value;
+    _weeklyCalorieBurned[key] = value;
+  }
+
+  void workoutDataInitialize(
+    Map<int, Map<int, int>> workoutDates,
+    Map<String, double> weeklyCaloriesBurned,
+  ) {
+    _workoutDates = workoutDates;
+    _weeklyCalorieBurned = weeklyCaloriesBurned;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'skill_level': skillLevel,
+      'age': age,
+      'height': height,
+      'weight': weight,
+      'exercise_days': exerciseDays,
+      'workout_dates': _workoutDates,
+      'weekly_calories_burned': _weeklyCalorieBurned,
+    };
   }
 }
