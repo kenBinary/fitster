f6d3af47d91c5f607188fdac86ed08732aa04084
thennek0 <kennethbecaro0@gmail.com>
2023-11-24 11:34:08 +0800

    fixed merged conflicts and minor code styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:fitster/loginpage.dart';
+import 'package:fitster/screens/loginpage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,7 +7,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
