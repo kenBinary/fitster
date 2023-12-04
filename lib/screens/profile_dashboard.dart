941ae613298397989c8ff59711d4e24794a73d54
thennek0 <kennethbecaro0@gmail.com>
2023-12-04 17:36:16 +0800

    can now read and initialize data from backend

--- lib/screens/profile_dashboard.dart
+++ lib/screens/profile_dashboard.dart
@@ -1,46 +1,100 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fitster/services/db_operations.dart';
 import 'package:flutter/material.dart';
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
+import 'package:gap/gap.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:widget_circular_animator/widget_circular_animator.dart';
+import 'package:d_chart/d_chart.dart';
 
-class ProfileDashboard extends StatelessWidget {
+class ProfileDashboard extends StatefulWidget {
   const ProfileDashboard({super.key});
-  Card _vitalCards(
-      {required String vitalType,
-      required String vitalValue,
-      required double fontSize}) {
+
+  @override
+  State<ProfileDashboard> createState() => _ProfileDashboardState();
+}
+
+class _ProfileDashboardState extends State<ProfileDashboard> {
+  Map<String, dynamic> initialData = {};
+  var displayName = '';
+  int daysExercised = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeData().then((data) {
+      User? user = FirebaseAuth.instance.currentUser;
+      if (data.isNotEmpty) {
+        setState(() {
+          if (user != null) {
+            final name = user.providerData[0];
+            displayName = name.displayName!;
+            initialData = data['user_information'];
+            daysExercised = data['days_exercised'];
+          } else {
+            initialData = data['user_information'];
+            daysExercised = data['days_exercised'];
+          }
+        });
+      }
+    });
+  }
+
+  Future<Map<String, dynamic>> initializeData() async {
+    List<dynamic> daysExercised =
+        await getDaysExercised(docId: 'uZoLJiYAwDUYpd17VLHttlETltf2');
+    Map<String, dynamic> userInformation =
+        await getUserInformation(docId: 'uZoLJiYAwDUYpd17VLHttlETltf2');
+    // return await getUserInformation(docId: 'uZoLJiYAwDUYpd17VLHttlETltf2');
+    return {
+      'days_exercised': daysExercised.length,
+      'user_information': userInformation,
+    };
+  }
+
+  String calculateBMI({required double height, required double weight}) {
+    return ((weight) / ((height / 100) * (height / 100))).toStringAsFixed(2);
+  }
+
+  double getBurnedCalorie({required String day}) {
+    return ((initialData.isNotEmpty &&
+            initialData.containsKey('weekly_burned_calories'))
+        ? initialData['weekly_burned_calories'][day]
+        : 0) as double;
+  }
+
+  Card _vitalCards({
+    required String vitalType,
+    required String vitalValue,
+  }) {
     return Card(
+      shape: BeveledRectangleBorder(
+        side: BorderSide(
+          color: HexColor('#9AB8F9'),
+          width: 1,
+        ),
+        borderRadius: BorderRadius.circular(2),
+      ),
       child: Padding(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(5),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              vitalType,
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                vitalType,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            Expanded(
-              child: Center(
-                child: AspectRatio(
-                  aspectRatio: 1 / 1,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(1000),
-                      ),
-                      color: Colors.blue,
-                    ),
-                    child: Center(
-                      child: Text(
-                        vitalValue,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: fontSize,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+            Text(
+              vitalValue,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
               ),
             )
           ],
@@ -53,106 +107,200 @@ class ProfileDashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            flex: 2,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(45),
-                    ),
-                    color: Colors.black,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: WidgetCircularAnimator(
+                innerColor: HexColor('#0C4ACF'),
+                outerColor: HexColor('#020A1D'),
+                child: CircularProfileAvatar(
+                  '',
+                  borderColor: HexColor('#9AB8F9'),
+                  borderWidth: 3,
+                  elevation: 5,
+                  radius: 90,
+                  child: Image.asset(
+                    '../lib/images/Push_Person.png',
                   ),
-                  height: 80,
-                  width: 80,
-                  // color: Colors.black,
                 ),
-                const SizedBox(
-                  width: 15,
-                ),
-                const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Jhon Doe',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Beginner',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500,
+              ),
+            ),
+            // Center(
+            //   child: CircularProfileAvatar(
+            //     '',
+            //     borderColor: HexColor('#9AB8F9'),
+            //     borderWidth: 3,
+            //     elevation: 5,
+            //     radius: 90,
+            //     child: Image.asset(
+            //       '../lib/images/Push_Person.png',
+            //     ),
+            //   ),
+            // ),
+            const Gap(10),
+            SizedBox(
+              height: 40,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        (FirebaseAuth.instance.currentUser != null)
+                            ? displayName
+                            : '',
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
+                  ),
+                  VerticalDivider(
+                    width: 20,
+                    thickness: 2,
+                    endIndent: 0,
+                    color: HexColor('#0C4ACF'),
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        (initialData.isNotEmpty)
+                            ? initialData['fitness_level'].toString()
+                            : '',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Gap(10),
+            const Text(
+              'User Information',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Divider(
+              thickness: 2,
+              color: HexColor('#0C4ACF'),
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: _vitalCards(
+                          vitalType: 'Age',
+                          vitalValue: (initialData.isNotEmpty)
+                              ? initialData['age'].toString()
+                              : ''),
+                    ),
+                    Expanded(
+                      child: _vitalCards(
+                          vitalType: 'Weight',
+                          vitalValue: (initialData.isNotEmpty)
+                              ? initialData['weight'].toString()
+                              : ''),
+                    ),
                   ],
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: _vitalCards(
-                      vitalType: 'Age', vitalValue: '23', fontSize: 20),
                 ),
-                Expanded(
-                  flex: 1,
-                  child: _vitalCards(
-                      vitalType: 'Weight', vitalValue: '70kg', fontSize: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      child: _vitalCards(
+                          vitalType: 'Height',
+                          vitalValue: (initialData.isNotEmpty)
+                              ? initialData['height'].toString()
+                              : ''),
+                    ),
+                    Expanded(
+                      child: _vitalCards(
+                          vitalType: 'BMI',
+                          vitalValue: (initialData.isNotEmpty)
+                              ? calculateBMI(
+                                  height: double.parse(initialData['height']),
+                                  weight: double.parse(initialData['weight']))
+                              : ''),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  flex: 1,
-                  child: _vitalCards(
-                      vitalType: 'Height', vitalValue: '1.73m', fontSize: 20),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: _vitalCards(
-                      vitalType: 'BMI', vitalValue: '23', fontSize: 30),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: _vitalCards(
-                      vitalType: 'Exercise Days',
-                      vitalValue: '70kg',
-                      fontSize: 30),
+                Row(
+                  children: [
+                    Expanded(
+                      child: _vitalCards(
+                          vitalType: 'Days Exercised',
+                          vitalValue: daysExercised.toString()),
+                    ),
+                    Expanded(
+                      child: _vitalCards(
+                          vitalType: 'Fitness Goals',
+                          vitalValue: (initialData.isNotEmpty)
+                              ? initialData['fitness_goals'].toString()
+                              : ''),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ),
-          const Text(
-            'Calories Burned This Week',
-            textAlign: TextAlign.end,
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
+            const Gap(10),
+            const Text(
+              'Calories Burned This Week',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 4,
-            child: Container(
-              color: Colors.blueAccent,
+            Divider(
+              thickness: 2,
+              color: HexColor('#0C4ACF'),
             ),
-          ),
-        ],
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: DChartBarO(
+                groupList: [
+                  OrdinalGroup(
+                    id: '1',
+                    data: [
+                      OrdinalData(
+                        domain: 'Mon',
+                        measure: getBurnedCalorie(day: 'monday'),
+                      ),
+                      OrdinalData(
+                          domain: 'Tue',
+                          measure: getBurnedCalorie(day: 'tuesday')),
+                      OrdinalData(
+                          domain: 'Wed',
+                          measure: getBurnedCalorie(day: 'wednesday')),
+                      OrdinalData(
+                          domain: 'Thu',
+                          measure: getBurnedCalorie(day: 'thursday')),
+                      OrdinalData(
+                          domain: 'Frid',
+                          measure: getBurnedCalorie(day: 'friday')),
+                      OrdinalData(
+                          domain: 'Sat',
+                          measure: getBurnedCalorie(day: 'saturday')),
+                      OrdinalData(
+                          domain: 'Sun',
+                          measure: getBurnedCalorie(day: 'sunday')),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
